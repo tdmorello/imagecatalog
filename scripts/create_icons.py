7b25a5f13a96d1521839f6884c58a92cb6033603
Tim Morello <tdmorello@gmail.com>
2022-01-05 21:04:08 -0500

    Add script to create sample images

---
+++ scripts/create_icons.py
@@ -0,0 +1,19 @@
+"""Script to create a set of images."""
+
+from pathlib import Path
+
+from PIL import Image, ImageDraw
+
+
+def text(output_path):
+    output_path = Path(output_path)
+    image = Image.new("RGB", (200, 200), "green")
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), output_path.name)
+    image.save(output_path)
+
+
+if __name__ == "__main__":
+    fnames = [Path(f"image_{i:02}.jpg") for i in range(1, 21)]
+    for name in fnames:
+        text(Path("./images") / name)
