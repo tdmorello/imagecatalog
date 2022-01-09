9808517d56c9ef42cc2586a7bfde830e275a3049
Tim Morello <tdmorello@gmail.com>
2022-01-09 17:41:46 -0500

    Update tests

--- tests/test_imagecatalog.py
+++ tests/test_imagecatalog.py
@@ -1,13 +1,34 @@
 """Test suite."""
 
-from pathlib import Path
+import subprocess
+import sys
 
+import pytest
 from PIL import Image
 
 from imagecatalog import Catalog, create_catalog
 
 
-def test_cli_from_files():
+@pytest.fixture
+def sample_image_dir(tmp_path):
+    # create sample data
+    for i in range(12):
+        fpath = tmp_path / f"image_{i:02}.jpg"
+        image = Image.new("RGB", (200, 200), "gray")
+        image.save(fpath)
+
+    return tmp_path
+
+
+def test_cli_from_files(sample_image_dir):
+
+    # image_dir = sample_image_dir
+    # output = sample_image_dir / "output.pdf"
+
+    # cmd = f"{sys.executable} -m imagecatalog -i {str(image_dir)} -f '*.jpg' --rows 4 --cols 3 {str(output)}"
+    # res = subprocess.run(cmd.split(), capture_output=True)
+
+    # assert output.exists()
     ...
 
 
@@ -15,23 +36,20 @@ def test_cli_from_csv():
     ...
 
 
-def test_catalog_from_files(tmp_path):
+def test_catalog_from_files(sample_image_dir):
     """Catalog can be created from files."""
 
     # create sample data
-    for i in range(12):
-        fpath = tmp_path / f"image_{i:02}.jpg"
-        image = Image.new("RGB", (200, 200), "gray")
-        image.save(fpath)
+    image_dir = sample_image_dir
 
     catalog = Catalog()
     catalog.set_title("test catalog")
-    images = [tmp_path / f"image_{i:02}.jpg" for i in range(12)]
+    images = [image_dir / f"image_{i:02}.jpg" for i in range(12)]
     labels = [f"Image {i}" for i in range(len(images))]
     notes = [f"image {i} note" for i in range(len(images))]
     catalog.add_page()
     catalog.build_table(images, labels, notes, 4, 3)
-    catalog.output(tmp_path / "catalog.pdf")
+    catalog.output(image_dir / "catalog.pdf")
 
     # TODO read back pdf, search text fields, count number of images
 
